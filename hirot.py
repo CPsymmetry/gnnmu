c99084d23aaee1768c90147317d7b86f8b38c4d1
CPsymmetry <73593071+CPsymmetry@users.noreply.github.com>
2023-07-21 13:47:05 +0100

    added comments to code

--- hirot.py
+++ hirot.py
@@ -10,11 +10,29 @@ path_input = "~/sim/lct_BIB.root"
 lam_max = 75/360 * 2*np.pi
 
 
-def multi_lkth(folder_path, max_events=10, step=1):
+def multi_lkth(folder_path, max_events=1, step=1):
+    """
+    Loops over all LCTuple files and retrieves its data
+    
+    Parameters
+    ----------
+    folder_path : string
+        path to the folder containing all the files.
+    max_events : int, optional
+        the max number of events to load through. The default is 10.
+    step : int, optional
+        The step size for looping over chunks of data. The default is 1.
+
+    Returns
+    -------
+    multi_log : list
+        A list of all the event data stored in dictionaries.
+    """
     multi_log = []
     num = 0
     for file in os.listdir(folder_path):
         path = f'{folder_path}/{file}'
+        #retrieves all keys in a root file and finds those labelled MYLCTuple.
         trees = [k for k in uproot.open(path).keys() if k.startswith('MyLCTuple')]
         for ttree in trees:
             for lctuple in uproot.iterate({f'{path}':ttree}, step_size=step):
@@ -27,7 +45,26 @@ def multi_lkth(folder_path, max_events=10, step=1):
     return multi_log
 
 def lkth(lctuple):
-      
+    """
+    Processes data that is needed from the file and compiles it into a dictionary for
+    ease of use.
+    
+    Parameters
+    ----------
+    lctuple : list
+        array of data to be processed.
+        
+    Returns
+    -------
+    dict
+        {log: dict of mc particle arrays,
+         ltr: dict of all track state arrays,
+         lti: dict of all track information arrays,
+         cov: array of the covariance matrix,
+         rf: fraction of data that passes gen==1 cha!=0 selection
+         }
+    """
+     
     log = {'mcmox':[],  #px 
            'mcmoy':[],  #py
            'mcmoz':[],  #pz
@@ -45,17 +82,17 @@ def lkth(lctuple):
     
     lti = {'trch2':[],  #chi2
            'trndf':[],  #degrees of freedom
-           'trsip':[],
-           'trsfh':[],
-           'trslh':[], 
-           'trsca':[],
-           'tr2mcf':[],
+           'trsip':[],  #Index of track state (ts branches) at interaction point
+           'trsfh':[],  #Index of track state (ts branches) at the first hit
+           'trslh':[],  #Index of track state (ts branches) at the last hit
+           'trsca':[],  #Index of track state (ts branches) at the calorimeter
+           'tr2mcf':[], #Track index from tr branches.
+           'tr2mct':[], #To matched MC particle in mc branches
            }
     
     #'tof':[],    #time of flight
     #MC particle parameters
     rf = 0
-    tn = 0
     
     for i in log:
         params = lctuple[i]
@@ -84,7 +121,10 @@ def lkth(lctuple):
     gen = log['mcgst']
     cha = log['mccha']
     
+    #selection that particles must be stable and must not have neutral charge
     pre = (gen==1) & (cha!=0)
+    
+    rf = ak.count(pre[pre])
     
     for i in log:
         log[i] = log[i][pre]
@@ -92,11 +132,13 @@ def lkth(lctuple):
     mx = log['mcmox']
     my = log['mcmoy']
     mz = log['mcmoz']
+    
+    #updates the dictionary with additional arrays of important variables
 
     log.update({'mcppt':ak.Array(np.sqrt(mx**2 + my**2 + mz**2))})
-    log.update({'mclambda':np.arctan2(mz,log['mcppt'])})
+    log.update({'mcl':np.arctan2(mz,log['mcppt'])})
     log.update({'mcphi':np.arctan2(my,mx)})
     
-    ltr.update({'trppt':(.3 * 3.57)/(omega[trsip])})
+    ltr.update({'tsppt':(.3 * 3.57)/(omega)})
     
     return {'log':log,'ltr':ltr,'lti':lti,'cov':covariance,'rf':rf}
