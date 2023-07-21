7743d93e63711e7350b03d6df0b52981562875cf
CPsymmetry <73593071+CPsymmetry@users.noreply.github.com>
2023-07-21 12:36:31 +0100

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -12,22 +12,13 @@ class INModel(snt.Module):
         size = 128
         
         self.gn_module = gn.modules.GraphNetwork(
-           edge_model_fn=lambda: snt.nets.MLP([size, size, 2]),
-           node_model_fn=lambda: snt.nets.MLP([size, size, 0]),
-           global_model_fn=lambda: snt.nets.MLP([size, size,0])        
+           edge_model_fn=lambda: snt.nets.MLP([size, size, 0]),
+           node_model_fn=lambda: snt.nets.MLP([size, size, 7]),
+           global_model_fn=lambda: snt.nets.MLP([size, size,2])        
            )          
        
     def __call__(self, data):
-        """
-        Takes data and parses it through the graph network
-        
-        data: dictionary, [formatted_graphs, node_system, true_path]
-        """
-        #print(f"dgraph data {data['dgraphs']}")
-        print(data['dgraphs'])
         gnn = self.gn_module(data['dgraphs'])
-        
-        
         return gnn
     
     
