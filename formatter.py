f00b9d08d52fada40996df198240fc74a997fa6f
CPsymmetry <73593071+CPsymmetry@users.noreply.github.com>
2023-07-21 12:35:36 +0100

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,87 +1,44 @@
 import numpy as np
-
 import graph_nets as gn
 import tensorflow as tf
+import itertools
 
-def format_graph(node_sys):
+def format_graph(system):
     """
-    Takes a node system and formats it into a graph_net compatible dictionary
+    Takes the system and formats it into a graph_net compatible dictionary
     """
-    globals_ = []
-    edge_list = []
-    senders = []
-    receivers = []
+    globals_ = system.globals
+    #chi2, ndf
+    nodes = system.nodes
     
-    for edge in node_sys.edges:
-        s = edge[0]
-        r = edge[1]
-        dist = edge[2]
-        senders.append(s.un)
-        receivers.append(r.un)
-        #converts floats to tf.float32
-        rd = tf.cast(dist, tf.float32)
-        edge_list.append(rd)
-            
-    nodes = [[]]*len(senders)*2
+    #pt, tanlambda, phi, d0, z0, cov, time_of_flight
+    edges = [[]]*system.len
     
-    edges = np.array([weight for weight in edge_list], ndmin=2).T
+    i=itertools.product(range(nodes.shape[0]),range(nodes.shape[0]))
+    senders=[]
+    receivers=[]
+    for s,r in i:
+        if s==r: continue
+        senders.append(s)
+        receivers.append(r)
+    edges=[[]]*len(senders)
     
     return {'globals':globals_, 'nodes':nodes, 'edges':edges, 'senders':senders, 
             'receivers':receivers}
 
 def format_graphs(dgraphs):
     """
-    Takes an array of formatted node systems and formats it ready to be used
+    Takes an array of formatted systems and formats it ready to be used
     by Graph_net
     """
     return gn.utils_tf.data_dicts_to_graphs_tuple(dgraphs)
 
-def formatter(num_samples, mimax=[3,5], size=[10,10], ef=0.0, truth_value=False):
-    """
-    
-    Parameters
-    ----------
-    num_samples : int
-        Number of node systems to sample
-    mimax : array, optional
-        Minimum and maximum values of nodes to sample from
-    size : array, optional
-        X and Y size of the canvas where the nodes lie. The default is [10,10].
-    ef : float, optional
-        Varies the number of node connections made. 0 - minimum connections,
-        1 - maximum connections. The default is 0.
-    truth_value : Boolean, optional
-        Whether to create the true path for each node system sampled. 
-        The default is False.
-
-    Returns
-    -------
-    dict
-        [formatted dgraphs, node_systems for each graph, true path for each graph].
-
-    """
-    from nodal import nodal
-    
-    node_systems = []
+def formatter(samples):
     dgraphs = []
-    path_systems = []
-    labels = []
     
-    for i in range(num_samples):
-        max_nodes = 3#np.random.randint(mimax[0],mimax[1])
-        node_sys = nodal(max_nodes, size, ef)
-        node_systems.append(node_sys)
-        
-        dgraphs.append(format_graph(node_sys))
-        
-        if truth_value:
-            path_systems.append(node_sys.shortestpath()) 
-        else:
-            path_systems.append({'dist':0, 'labels':[], 'path':[]})
-     
-    for path_system in path_systems:
-        labels+=path_system['labels']
+    for i in samples:
+        dgraphs.append(format_graph(i)) 
                 
     dgraphs = format_graphs(dgraphs)
     
-    return {'dgraphs':dgraphs, 'node_systems':node_systems, 'path_systems':path_systems, 'labels':labels}
+    return {'dgraphs':dgraphs}
