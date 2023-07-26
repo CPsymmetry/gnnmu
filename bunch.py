7596420a159092b114ff9930a6eb2e6b1ebab83a
CPsymmetry <73593071+CPsymmetry@users.noreply.github.com>
2023-07-26 16:34:26 +0100

    Update bunch.py

--- bunch.py
+++ bunch.py
@@ -1,6 +1,6 @@
 import awkward as ak
 import numpy as np
-import uproot
+import pandas as pd
 import matplotlib.pyplot as plt
 from hirot import multi_lkth
 
@@ -13,7 +13,7 @@ from hirot import multi_lkth
            'mcpdg':[],  #pdg
            'mcppt':[],   #momentum
            'mcphi':[],  #phi
-           'mcl':[]     #lambda
+           'mclam':[]     #lambda
            }
     
     ltr = {'tstnl':[],  #tan(lambda)
@@ -22,6 +22,7 @@ from hirot import multi_lkth
            'tsdze':[],  #d0
            'tszze':[],  #z0
            'tsppt':[],  #track state momentum
+           'tscov':[],  #covariance
            }
     
     lti = {'trch2':[],  #chi2
@@ -30,80 +31,159 @@ from hirot import multi_lkth
            'trsfh':[],  #Index of track state (ts branches) at the first hit
            'trslh':[],  #Index of track state (ts branches) at the last hit
            'trsca':[],  #Index of track state (ts branches) at the calorimeter
-           'tr2mcf':[], #Track index from tr branches.
-           'tr2mct':[], #To matched MC particle in mc branches
+           'mc2trf':[], #Track index from tr branches.
+           'mc2trt':[], #To matched MC particle in mc branches
            }
 """
+
+lam_max = 75/360 * 2*np.pi
 
 class bunch:
     def __init__(self, sys):
         self.log = sys['log']
         self.ltr = sys['ltr']
         self.lti = sys['lti']
-        self.covariance = sys['cov']
         self.rf = sys['rf']
-        self.trsip = self.lti['trsip']
-        self.events = []
-        self.match_tracks()   
+        print(len(self.lti['trndf'][0]))
+        print(len(self.ltr['tstnl'][0]))
+        self.ltg = {'trch2':ak.flatten(sys['lti']['trch2']),
+                    'trndf':ak.flatten(sys['lti']['trndf'])}
+        self.events = pd.DataFrame([])
+        
+    def graphs_net_setup(self):
+        mc2trt = self.lti['mc2trt'][0]
+        mc2trf = self.lti['mc2trf'][0]
+        
+        self.globals = ak.to_numpy(ak.zip(self.ltg.values())).view(('<f4',2))
+        self.nodes = ak.to_numpy(ak.zip(self.ltr.values())).view(('<f4',6))[0]
+        #self.globals = self.nodes
+        self.senders = ak.to_numpy(ak.concatenate([*self.lti['trsip'],*self.lti['trsfh'],*self.lti['trslh']]))
+        self.receivers = ak.to_numpy(ak.concatenate([*self.lti['trsfh'],*self.lti['trslh'],*self.lti['trslh']]))
+        self.edges = np.array([[]]*len(self.senders))
+        
+        self.labels = np.array([[0.,1.]]*len(self.globals))
+        
+        trsip = self.lti['trsip']
+        inter,n1,n2 = np.intersect1d(ak.flatten(trsip),mc2trt, return_indices=True)
+        self.labels[mc2trf[n2]] = [1.,0.]
+        
+        """
+        print(self.globals)
+        print(self.nodes)
+        print(self.senders)
+        print(self.receivers)
+        print(self.edges)
+        """
     
-    def match_tracks(self):
-        tr2mcf = self.lti['tr2mcf']
-        tr2mct = self.lti['tr2mct']
-        
-        tsphi = self.ltr['tsphi'][tr2mcf]
-        tsl = np.arctan(self.ltr['tstnl'][tr2mcf])
-        tsppt = self.ltr['trppt'][tr2mcf]
-        
-        mcppt = self.log['mcppt'][tr2mct]
-        mcphi = self.log['mcphi'][tr2mct]
-        mcl = self.log['mcl'][tr2mct]
-        
-        self.dif = {'dif_ppt':mcppt-tsppt,'dif_lambda':mcl-tsl,'dif-phi':mcphi-tsphi}
-        
-        self.plot_bunch({'dif_ppt':None,'dif_lambda':None, 'dif_phi':None}, maximum=1)
     
-    def plot_bunch(self, args, maximum):
-        for arg in args.keys():
-            x = arg         
-            val = args[arg]
-            xlabel = ''
+    
+    def match_tracks(self, selection = True):
+        mc2trt = self.lti['mc2trt']
+        mc2trf = self.lti['mc2trf']
+        
+        track_state_info = {'tsppt':'ppt','tsphi':'phi','tstnl':'lam','tsome':'omega','tsdze':'d0','tszze':'z0'}
+        track_info = {'trch2':'ch2','trndf':'ndf'}
+        
+        pre = self.log['mcgst'][mc2trf] != None
+        
+        if selection:
+            gen = self.log['mcgst'][mc2trf]
+            cha = self.log['mccha'][mc2trf]
+            lam = self.log['mclam'][mc2trf]
+        #selection that particles must be stable and must not have neutral charge + lambda < 75 degrees
+            pre = (gen==1) & (cha!=0) & (lam<lam_max)  
+        
+        for x in track_state_info:
+            col = track_state_info[x]
+            self.events[col] = ak.flatten(self.ltr[x][mc2trt][pre])
+            
+        for x in track_info:
+            col = track_info[x]
+            self.events[col] = ak.flatten(self.lti[x][mc2trt][pre])
+        
+    def plot_bunch(self, labels, globels={}, bunches=None):
+        
+        bunches = self or bunches        
+        for x in labels.keys():   
             flt = []
-            oper = lambda x : x
-            if val is not None:
-                oper = val
-                
+            local = labels[x]
+            
+            local_keys = local.keys()
+            global_keys = globels.keys()
+            
+            xlabel = ''
+            ylabel = ''
+            ran = (-10000,10000)
+            bins = 1000
+            operation = lambda x : x
+            
+            for key in local_keys:
+                if 'xlabel' in key:
+                    xlabel = local[key]
+                elif 'ylabel' in key:
+                    ylabel = local[key]
+                elif 'range' in key:
+                    ran = local[key]
+                elif 'bins' in key:
+                    bins = local[key]
+            
+            for key in global_keys:
+                if 'xlabel' in key:
+                    xlabel = globels[key]
+                elif 'ylabel' in key:
+                    ylabel = globels[key]
+                elif 'range' in key:
+                    ran = globels[key]
+                elif 'bins' in key:
+                    bins = globels[key]
+
             if 'mc' in x:
                 flt = ak.flatten(self.log[x], axis=None)
-            elif 'dif' in x:
-                flt = np.array(self.dif[x])
             else:
-                flt = ak.flatten(self.ltr[x], axis=None)
+                flt = np.array(self.events[x])
             
             if 'phi' in x or 'lambda' in x:
                 xlabel = 'radians'
             
-            print(flt)
-            plt.hist(oper(flt[(flt<maximum) & (flt>-maximum)]), bins=1000)
+            mini, maxi = ran
+            
+            plt.hist(operation(flt[(flt<maxi) & (flt>mini)]), bins=bins)
             plt.title(label=x)
             plt.yscale('log')
             plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
             plt.show()
-            
-    """
+     
     @staticmethod
-    def find_minimum(arg1,arg2):
-          cross_prod = ak.cartesian([arg1,arg2])
-          a,b = ak.unzip(cross_prod)
-          dif = np.abs(a-b)
-          return ak.min(dif), dif   
+    def assign_bunches(folder_path, max_events=1, step=1):
+        mlkth = multi_lkth(folder_path, max_events=max_events, step=step)
+        bunches = []
+        for i in mlkth:
+            bunches.append(bunch(i))
+            
+        return bunches
+    
+    @staticmethod 
+    def trackPerf_to_bunch(folder_path, max_events=1, step=1):
+        mlkth = multi_lkth(folder_path, max_events=max_events, step=step, track_perf=True)
+        bunches = []
+        for i in mlkth:
+            bun = bunch(i)
+            bunches.append(bun)
+            bun.match_tracks()
+            bun.graphs_net_setup()
+            
+        return bunches
+
+
+if __name__ == '__main__': 
+    samples = bunch.trackPerf_to_bunch('/home/kali/sim/data')
+
     """
-
-def assign_bunches(folder_path, max_events=1, step=1):
-    mlkth = multi_lkth(folder_path, max_events=max_events, step=step)
-    bunches = []
-    for i in mlkth:
-        bunches.append(bunch(i))
-        
-    return bunches
-
-bunches = assign_bunches('/home/kali/sim/data')
+    bunches[0].plot_bunch(labels = 
+                          {'ppt':{'xlabel':'true - reconstructed $p_T$ [MeV]'},
+                           'lambda':{'xlabel':'true - reconstructed lambda [radians]'}, 
+                           'phi':{'xlabel':'true - reconstructed phi [radians]'}
+                           }, 
+                          globels = {'range':(-1,1), 'ylabel':'count', 'bins':100})
+    """
