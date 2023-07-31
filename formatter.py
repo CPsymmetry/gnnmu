09d088eff2dc89014f8b3b0dbb48524e03d30011
CPsymmetry <73593071+CPsymmetry@users.noreply.github.com>
2023-07-31 10:52:58 +0100

    Update formatter.py
    commented

--- formatter.py
+++ formatter.py
@@ -2,16 +2,17 @@ import graph_nets as gn
 import tensorflow as tf
 import numpy as np
 
-def format_graph(system):
+def format_graph(glob, nodes, edges, senders, receivers):
     """
     Takes the system and formats it into a graph_net compatible dictionary
     """
     
-    return {'globals':tf.convert_to_tensor(system.globals), 
-            'nodes':tf.convert_to_tensor(system.nodes), 
-            'edges':tf.convert_to_tensor(system.edges), 
-            'senders':tf.convert_to_tensor(system.senders), 
-            'receivers':tf.convert_to_tensor(system.receivers)}
+    return {'globals':tf.convert_to_tensor(glob,dtype=tf.float32), 
+            'nodes':tf.convert_to_tensor(nodes,dtype=tf.float32), 
+            'edges':tf.convert_to_tensor(edges,dtype=tf.float32), 
+            'senders':tf.convert_to_tensor(senders,dtype=tf.int32), 
+            'receivers':tf.convert_to_tensor(receivers,dtype=tf.int32)
+            }
 
 def format_graphs(dgraphs):
     """
@@ -20,17 +21,65 @@ def format_graphs(dgraphs):
     """
     return gn.utils_tf.data_dicts_to_graphs_tuple(dgraphs)
 
-def formatter(samples):
-    dgraphs = []
-    labels = None
-    for i in samples:
-        dgraphs.append(format_graph(i)) 
-        if labels is None:
-            labels = i.labels
-        else:
-            labels = np.concatenate((labels,i.labels))
+def formatter(samples, sample_size=1, graph_size=1):
+    """
+    Formats the data into GraphTuples for the GNN
     
+    Parameters
+    ----------
+    samples : array
+        a list of bunches.
+    sample_size : int, optional
+        The maximum number of tracks to be used. The default is 1.
+    graph_size : TYPE, optional
+        The maximum number of tracks to be used for a single graph. The default is 1.
+
+    Returns
+    -------
+    dgraph data : {GraphTuple, array, array}
+        All the graph data that is needed for the GNN, including the original samples.
+
+   """
+    dgraphs = []
+    n = 0   #num. of tracks added
+    
+    for v in samples:
+        ng = 0
+        ns = 0
+        senders = v.senders
+        receivers = v.receivers
+        arg_len = len(v.globals)
+        
+        for i in np.arange((sample_size)/graph_size):
+            difng = (graph_size+ns)-arg_len
+            print()
+            edges = np.array([[0],[1]])*graph_size
+            
+            labels = np.empty((0,2),dtype=float)
+            nodes = np.empty((0,3,21),dtype=float)
+            glob = np.empty((0,2),dtype=float)
+            
+            if difng < 0:
+                ran = np.arange(ns, (graph_size+ns))  
+                
+                labels = np.append(labels,v.labels[ran],axis=0)
+                nodes = np.append(nodes,v.nodes[ran],axis=0)
+                glob = np.append(glob,v.globals[ran],axis=0)
+                
+                ng+=graph_size
+                ns+=ng
+                n+=ng  
+                print(f'n:{n},  ns:{ns}')
+                print(glob)
+                print(nodes)
+                print(labels)
+                
+                dgraphs.append(format_graph(glob, nodes, edges, senders, receivers)) 
+            else:
+                break
+            
+        
+    print(dgraphs)   
     dgraphs = format_graphs(dgraphs)
-    print(dgraphs)
     
     return {'dgraphs':dgraphs, 'sys':samples, 'labels':labels}
